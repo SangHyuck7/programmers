fcdb83276617e06f4a7b823244f825d60b0c449f
SangHyuck <107290583+SangHyuck7@users.noreply.github.com>
2025-04-07 15:52:03 +0900

    [level 2] Title: 구명보트, Time: 37.10 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,19 @@
+//투포인터
+function solution(people, limit) {
+    people.sort((a, b) => a - b);
+    let start = 0;
+    let end = people.length - 1;
+    let boats = 0;
+    
+    
+    while (start <= end) {
+        if (people[start] + people[end] <= limit) {
+            start++;
+        } 
+        
+        end--;
+        boats++;
+    }
+    
+    return boats;
+}
